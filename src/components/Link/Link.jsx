35d669296c21e0112d7da7b07b133fee0fa1c959
emran <shuvoorba28@gmail.com>
2023-09-18 18:50:45 +0600

    navbar icon added

--- src/components/Link/Link.jsx
+++ src/components/Link/Link.jsx
@@ -4,9 +4,9 @@ import PropTypes  from "prop-types";
 const Link = ({route}) => {
     const {path, name} = route
     return (
-        <li className="mr-10">
+        <button className="mr-10">
                     <a href={path}>{name}</a>
-                </li>
+                </button>
     );
 };
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import Link from "../Link/Link";
+import { BiMenu } from 'react-icons/bi';
+import { TbArrowsCross } from 'react-icons/tb'
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
 
     const routes = [
         { id: 1, path: '/', name: 'Home' },
@@ -8,16 +13,24 @@ const Navbar = () => {
         { id: 3, path: '/services', name: 'Services' },
         { id: 4, path: '/contact', name: 'Contact' },
         { id: 5, path: '/help', name: 'help' }
-      ];
-      
+    ];
+
     return (
-        <div>
-            <ul className="flex">
-            {
-                routes.map(route => <Link route={route} key={route.id}></Link> )
-            }    
-            </ul>                  
-        </div>
+        <nav className=" p-5">
+            <div className="md:hidden text-3xl" onClick={() => setOpen(!open)}>
+                {
+                    open === true ?
+                    <TbArrowsCross></TbArrowsCross> :
+                    <BiMenu></BiMenu>
+                }
+                
+            </div>
+            <ul className="md:flex absolute md:static bg-rose-500">
+                {
+                    routes.map(route => <Link route={route} key={route.id}></Link>)
+                }
+            </ul>
+        </nav>
     );
 };
 
